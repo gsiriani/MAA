6ec742bea49db590e5c6c161b3a770019922bda1
Bruno Garate <bruno.garate@gmail.com>
2016-08-14 15:18:16 -0300

    Fixes y arreglos

--- Practico1/ajedrez.py
+++ Practico1/ajedrez.py
@@ -14,7 +14,7 @@ class Turno:
 
     @staticmethod
     def otroTurno(turno):
-        return Turno.BLANCA if turno == Turno.NEGRA else Turno.BLANCA
+        return Turno.BLANCA if turno == Turno.NEGRA else Turno.NEGRA
 
 class Direccion:
     BLANCA = 1
@@ -23,62 +23,85 @@ class Direccion:
 class Movimiento:
 
     def __init__(self, origen, destino):
-        self.Origen = origen
+        self.origen = origen
 
         if isinstance(destino,list):
-            self.Destino = destino
+            self.destino = destino
         else:
-            self.Destino = [destino]
+            self.destino = [destino]
 
-    Origen = (-1,-1)
-    Destino = []
+    origen = None
+    destino = []
 
-class Tablero:
-    def iniciar_tablero(self):
-        # Retorna un tablero inicial
+    def esComida(self):
+        return  abs(self.origen[0] - self.destino[0][0]) > 1
+
+class Damas:
+
+    movimientosValidosCalculados = []
+
+    def __init__(self, tablero = None, turno = None):
+        self.turno = turno if turno is not None else Turno.BLANCA
+
+        if tablero is not None:
+            self.tablero = tablero
+        else:
+            self.tablero = Damas.tableroVacio()
+
+        self.movimientosValidosCalculados = self.calcularMovimientosValidos()
+
+    @staticmethod
+    def tableroVacio():
 
         # Declaro la variable
-        self.tablero = [[0 for x in range(8)] for y in range(8)]
+        tablero = [[0 for x in range(8)] for y in range(8)]
 
         for x in range(8):
             for y in range(8):
                 if ((x + y) % 2) == 0:
-                    self.tablero[x][y] = Casilla.VACIA
+                    tablero[x][y] = Casilla.VACIA
                 else:
-                    self.tablero[x][y] = Casilla.INVALIDA
+                    tablero[x][y] = Casilla.INVALIDA
 
-    def tablero_base(self):
+        return tablero
+
+    @staticmethod
+    def tablero_base():
+
+        tablero = Damas.tableroVacio()
 
         # Completo las primeras 3 hileras con fichas blancas
         for x in range(3):
             for y in range(8):
                 if ((x + y) % 2) == 0:
-                    self.tablero[x][y] = Casilla.BLANCA
+                    tablero[x][y] = Casilla.BLANCA
 
         # Completo las primeras 3 hileras con fichas blancas
         for x in range(5, 8):
             for y in range(8):
                 if ((x + y) % 2) == 0:
-                    self.tablero[x][y] = Casilla.NEGRA
+                    tablero[x][y] = Casilla.NEGRA
 
-    def esMovimientoValido(self, movimiento, turno):
+        return tablero
 
-        x, y = movimiento.Destino
+    def esMovimientoValido(self, movimiento):
+
+        x, y = movimiento.origen
 
         if x < 0 or x > 7 or y < 0 or y > 7:
             return False
 
         colorEnPosicion = self.tablero[x][y]
 
-        if(colorEnPosicion != turno):
+        if(colorEnPosicion != self.turno):
             return False
 
         # Sólo comidas simples
 
-        if len(movimiento.Destino) != 1:
+        if len(movimiento.destino) != 1:
             return False
 
-        xDest, yDest = movimiento.Destino[0]
+        xDest, yDest = movimiento.destino[0]
 
         if xDest < 0 or xDest > 7 or yDest < 0 or yDest > 7:
             return False
@@ -86,13 +109,13 @@ class Tablero:
         difX = xDest - x
         difY = yDest - y
 
-        if abs(difX) != abs(difY):
+        if difX == 0 or abs(difX) != abs(difY):
             return False
 
         signoX = difX/abs(difX)
 
-        if (Turno.BLANCA and signoX != Direccion.BLANCA) or\
-                (Turno.NEGRA and signoX != Direccion.BLANCA):
+        if (self.turno == Turno.BLANCA and signoX != Direccion.BLANCA) or\
+                (self.turno == Turno.NEGRA and signoX != Direccion.NEGRA):
             return False
 
         if difX == 0:
@@ -104,5 +127,69 @@ class Tablero:
         medioX = (xDest + x) / 2
         medioY = (yDest + y) / 2
 
-        return difX == 1 or self.tablero[medioX][medioY] == Turno.otroTurno(turno)
+        return abs(difX) == 1 or self.tablero[medioX][medioY] == Turno.otroTurno(self.turno)
 
+    def obtenerTableroResultante(self,movimiento):
+
+        if not self.esMovimientoValido(movimiento):
+            return Damas(self.tablero, self.turno)
+
+        tablero = self.tablero
+
+        x,y = movimiento.origen
+        tablero[x][y] = Casilla.VACIA
+
+        for destino in movimiento.destino:
+            xDest, yDest = destino
+            if abs(xDest - x) > 1:
+                tablero[(xDest + x) / 2][(yDest + y) / 2] = Casilla.VACIA
+
+            x,y = xDest, yDest
+
+        tablero[xDest][yDest] = self.turno
+
+        return Damas(tablero, Turno.otroTurno(self.turno))
+
+
+    def calcularMovimientosValidos(self):
+
+        movimientos = []
+
+        for x in range(8):
+            for y in range(8):
+                if self.tablero[x][y] == self.turno:
+                    movimientos += self.calcularMovimientosValidosDePieza(x,y)
+
+        if any(m.esComida() for m in movimientos):
+            movimientos = [m for m in movimientos if m.esComida()]
+
+        return movimientos
+
+    def partidaTerminada(self):
+
+        return len(self.movimientosValidosCalculados) == 0
+
+    def movimientosValidosDePieza(self, x, y):
+
+        return [m for m in self.movimientosValidosCalculados if m.origen[0] == x and m.origen[1] == y]
+
+    def calcularMovimientosValidosDePieza(self, x,y):
+
+        movimientos = []
+
+        for xDest in range(8):
+
+            difX = (xDest - x)
+
+            movimiento = Movimiento((x, y), (xDest, y + difX))
+
+            if self.esMovimientoValido(movimiento):
+                movimientos.append(movimiento)
+
+            movimiento = Movimiento((x, y), (xDest, y - difX))
+
+            if self.esMovimientoValido(movimiento):
+                movimientos.append(movimiento)
+
+
+        return movimientos
