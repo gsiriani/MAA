db48ba2a821a1aae9b80bacf193d4b8554e50707
Bruno Garate <bruno.garate@gmail.com>
2016-08-16 11:10:27 -0300

    - Trabajando sobre LMS

--- Practico1/cli-learn.py
+++ Practico1/cli-learn.py
@@ -1,11 +1,12 @@
 # -*- coding: UTF-8 -*-
 
-from learn import Learner, Representacion, Maquina
+from learn import Learner, Representacion, Maquina, MaquinaAzar
 
 maquinaA = Maquina(Representacion())
 maquinaB = Maquina(Representacion())
+maquinaC = MaquinaAzar(Representacion())
 
-learner = Learner(maquinaA,maquinaB,0.001,False)
-learner.run(2000)
-learner = Learner(maquinaA,maquinaB,0.001,False)
+learner = Learner(maquinaA,maquinaB,0.0001,True)
+learner.run(10000)
+learner = Learner(maquinaA,maquinaC,0.0001,False)
 learner.run(1000, True)
--- Practico1/learn.py
+++ Practico1/learn.py
@@ -115,9 +115,6 @@ class Learner():
 
         weights = list(self.maquinaA.weights)
 
-        # TODO: Revisar! Esto evita que se disparen los pesos de errores
-        decisiones = [decisiones[-1]]
-
         for i,decision in enumerate(decisiones):
             for j,w in enumerate(weights):
 
@@ -224,6 +221,42 @@ class Maquina:
 
         return resultadoElegido
 
+
+class MaquinaAzar:
+
+    representacion = None
+
+    def __init__(self, representacion):
+
+        self.representacion = representacion
+        self.weights = tuple(0 for i in range(representacion.size + 1))
+
+    def valorTablero(self, representacion, damas):
+        if damas.partidaTerminada():
+            return self.valorTableroFinal(damas.tablero)
+        else:
+            return 0
+
+    def valorTableroFinal(self, tablero):
+        flattened = [casilla for fila in tablero for casilla in fila]
+
+        cantidadBlancas = sum([1 for c in flattened if c == Casilla.BLANCA])
+        cantidadNegras = sum([1 for c in flattened if c == Casilla.NEGRA])
+
+        return 0 if cantidadNegras == cantidadBlancas else (100 if cantidadBlancas > cantidadNegras else -100)
+
+
+    def decidirProximaJugada(self, damas):
+        movimientosPosibles = []
+
+        m = random.choice(damas.movimientosValidosCalculados)
+
+        resultado = Resultado()
+        resultado.estadoResultante = damas.obtenerTableroResultante(m)
+        resultado.representacionTablero = self.representacion.obtener(resultado.estadoResultante.tablero)
+        resultado.valorTableroResultante = self.valorTablero(resultado.representacionTablero, resultado.estadoResultante)
+
+        return resultado
 
 class Resultado:
     valorTableroAlInicioDelTurno = None
