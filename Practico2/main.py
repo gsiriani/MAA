1aceba6bae7702784ded856096e0027415f426e4
Guillermo Siriani <gsiriani@fing.edu.uy>
2016-09-05 17:54:41 -0300

    Corregida validacion cruzada para usar 10 bloques en lugar de bloques de tamano 10

--- Practico2/main.py
+++ Practico2/main.py
@@ -1,8 +1,8 @@
 from estudiante import Atributo,EstudianteBuilder
 from arbol import *
 from id3 import Id3
-from dibujante import Dibujante
 from random import shuffle
+# from dibujante import Dibujante # Utilizado para dibujar el arbol. Requiere bibliotecas extra
 
 builder = EstudianteBuilder()
 
@@ -16,7 +16,7 @@ builder.registrarAtributos(["school","sex","age","address",
                             "G1","G2"])
 builder.registrarAtributo("G3", lambda x: int(x)/5)
 builder.registrarAtributo("absences", lambda x: ('Nerd' if int(x) < 20 else 'Falton'))
-estudiantes = builder.obtenerEstudiantes("DataSets\student-por.csv")
+estudiantes = builder.obtenerEstudiantes("DataSets/student-por.csv")
 print(str(len(estudiantes)) + " estudiantes cargados")
 
 atributos = builder.atributos.keys()
@@ -30,9 +30,13 @@ id3 = Id3("G3")
 arbol = id3.ejecutar(estudiantes, atributos)
 arbol.imprimirEstadisticas()
 print("Relacion hojas/ejemplos: " + str(float(arbol.cantidadHojas())/len(estudiantes)))
-Dibujante.dibujar(arbol)
+# Dibujante.dibujar(arbol)
 
-# Validacion cruzada
+# ------------------
+# VALIDACION CRUZADA
+# ------------------
+
+print "Validacion cruzada..."
 
 # Separo 1/5 de los casos de prueba
 cantEstTest = len(estudiantes)/5
@@ -42,19 +46,31 @@ shuffle(estudiantes)
 estudiantesTest = estudiantes[:cantEstTest]
 estudiantesEntrenamiento = estudiantes[cantEstTest + 1:]
 
-TAMANO_BLOQUE = 10
 
-cantBloques = len(estudiantesEntrenamiento)/TAMANO_BLOQUE
+CANT_BLOQUES = 10
+
+largoBloque = len(estudiantesEntrenamiento)/10
+
 resultados = []
 
-for i in range(cantBloques):
-    pos = i*TAMANO_BLOQUE
-    estTest = estudiantesEntrenamiento[pos:pos+TAMANO_BLOQUE]
-    estEntr = estudiantesEntrenamiento[:pos] + estudiantesEntrenamiento[pos+TAMANO_BLOQUE:]
+for i in range(CANT_BLOQUES):
+    pos = i*largoBloque
+    estTest = estudiantesEntrenamiento[pos:pos+largoBloque]
+    estEntr = estudiantesEntrenamiento[:pos] + estudiantesEntrenamiento[pos+largoBloque:]
     arbol = id3.ejecutar(estEntr,atributos)
-    #print(estTest)
     resultados.append(arbol.validarLista(estTest, "G3"))
-    #print ("Resultado validacion " + str(i) + ": " + str(resultados[i] * 100) + "%")
 
-error = sum([r/cantBloques for r in resultados])
-print ("Error medio: " + str(1 - error))
+aciertos = sum([r/CANT_BLOQUES for r in resultados])
+print ("Error medio: " + str(1 - aciertos))
+
+
+# ------------------
+# EVALUACION FINAL
+# ------------------
+
+print "Evaluacion final..."
+
+arbol = id3.ejecutar(estudiantesEntrenamiento, atributos)
+aciertos = arbol.validarLista(estudiantesTest, "G3")
+print "Error: " + str(1 - aciertos)
+
