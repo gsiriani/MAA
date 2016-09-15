c98c2d4f10eec4ec9d5924866049d06c0a21d804
Guillermo Siriani <gsiriani@fing.edu.uy>
2016-09-15 17:02:29 -0300

    Correcion de algoritmo bayes e implementacion de las pruebas

--- Practico3/bayes.py
+++ Practico3/bayes.py
@@ -5,82 +5,100 @@ class Bayes:
 	Clase que implementa el aprendizaje bayesiano
 	''' 
 
-	def __init__(self, atributoObjetivo, valoresPosibles):
-        '''
-        La clase se inicializa con el atributo objetivo que sera buscado
-        por el algoritmo
-        '''
-        self.atributoObjetivo = atributoObjetivo
-        self.valoresPosibles = valoresPosibles
+	def __init__(self, atributoObjetivo, atributos, valoresPosibles):
+		'''
+		La clase se inicializa con el atributo objetivo que sera buscado
+		por el algoritmo
+		'''
 
-    def ejecutar(self, ejemplos, atributos, ajuste = 1):
-        '''
+		self.atributoObjetivo = atributoObjetivo
+		self.atributos = atributos
+		self.valoresPosibles = valoresPosibles
+
+	def ejecutar(self, ejemplos, ajuste = 1):
+		'''
         Se ejecuta el algoritmo sobre la lista de datos "ejemplos", considerando los
         atributos "atributos".
         El algoritmo calcula la probabilidad de cada valor de cada atributo y la probabilidad condicionada
         de dicho valor en funcion del valor del atributo objetivo.
         El parametro "ajuste" indica la cantidad de ejemplos a agregar para cada valor en caso de que no 
         haya ningun ejemplo para algun valor posible de un atributo.
-        '''
+		'''
 
         # Calculo cantidad total de ejemplos
-        cantidadEjemplos = len(ejemplos)
+		cantidadEjemplos = len(ejemplos)
 
         # Inicializo el diccionario de probabilidades
-        probabilidades = {}
-        probabilidadesCondicionadas = {}
+		probabilidades = {}
+		probabilidadesCondicionadas = {}
 
-        for (nomAtributo, valPosibles) in atributos:
-        	probabilidades[nomAtributo] = {}
-        	probabilidadesCondicionadas[nomAtributo] = {}
+		atributos = self.atributos
 
-        	ajustarCantidad = False
+		for nomAtributo in atributos:
+			valPosibles = self.valoresPosibles[nomAtributo]
+			probabilidades[nomAtributo] = {}
+			probabilidadesCondicionadas[nomAtributo] = {}
+
+			ajustarCantidad = False
 
         	# Obtengo cantidad de apariciones de cada valor
-        	cantidades = {}
+			cantidades = {}
 			for v in valPosibles:
 				cantidadTotal = len([e for e in ejemplos if e[nomAtributo] == v])
 				cantidades[v] = {'total': cantidadTotal, 'totalCondicionada': cantidadTotal, 'condicionada': {}}
-				if cantidadTotal == 0
+				if cantidadTotal == 0:
 					ajustarCantidad = True
 
 				# Obtengo cantidad condicionada
 				ajustarCantidadCondicionada = False
-				for p in self.valoresPosibles:
-					cantCond = len([e for e in ejemplos if e[nomAtributo] == v and e[atributoObjetivo] == p])
+				for p in self.valoresPosibles[self.atributoObjetivo]:
+					cantCond = len([e for e in ejemplos if e[nomAtributo] == v and e[self.atributoObjetivo] == p])
 					cantidades[v]['condicionada'][p] = cantCond
 					if cantCond == 0:
 						ajustarCantidadCondicionada = True
 				# Si es necesario, agrego ejemplos condicionados al atributo objetivo
 				if ajustarCantidadCondicionada:
-					for p in self.valoresPosibles:
+					for p in self.valoresPosibles[self.atributoObjetivo]:
 						cantidades[v]['condicionada'][p] += ajuste
-					cantidades[v]['totalCondicionada'] += ajuste*len(self.valoresPosibles)
+					cantidades[v]['totalCondicionada'] += ajuste*len(self.valoresPosibles[self.atributoObjetivo])
 
 			cantidadEjemplosAtributo = cantidadEjemplos
 			# Si es necesario, agrego ejemplos
 			if ajustarCantidad:
 				for v in valPosibles:
-					cantidades[v] += ajuste
+					cantidades[v]["total"] += ajuste
 				cantidadEjemplosAtributo = cantidadEjemplos + ajuste*len(valPosibles)
 
 			# Calculo probabilidades del atributo
 			for v in valPosibles:
-				probabilidades[nomAtributo][v] = float(cantidades[v]/cantidadEjemplosAtributo)
+				probabilidades[nomAtributo][v] = float(cantidades[v]["total"])/cantidadEjemplosAtributo
 				probabilidadesCondicionadas[nomAtributo][v] = {}
-				for p in self.valoresPosibles:
-					probabilidadesCondicionadas[nomAtributo][v][p] = float(cantidades[v]['condicionada'][p] / cantidades[v]['totalCondicionada'])
+				for p in self.valoresPosibles[self.atributoObjetivo]:
+					probabilidadesCondicionadas[nomAtributo][v][p] = float(cantidades[v]['condicionada'][p]) / cantidades[v]['totalCondicionada']
 
 		return (probabilidades, probabilidadesCondicionadas)
 
 
-	def validar(self, ejemplo, probabilidades, probabilidadesCondicionadas):
+	def validarLista(self, lista, probabilidades, probabilidadesCondicionadas):
+		'''
+		Retorna el promedio de aciertos en la evaluacion de la lista de ejemplos segun
+		 las probabilidades calculadas
+		'''
+		correctos = 0
+		for e in lista:
+			estimado = self.evaluar(e, probabilidades, probabilidadesCondicionadas)
+			if estimado == e[self.atributoObjetivo]:
+				correctos += 1
+		return float(correctos)/len(lista)
+
+
+	def evaluar(self, ejemplo, probabilidades, probabilidadesCondicionadas):
 		'''
 		Retorna el valor mas probable para el atributo objetivo, dadas las probabilidades previamente calculadas
 		'''
 
 		p = {} # Diccionario de probabilidades para el ejemplo
-		for valor in self.valoresPosibles:
+		for valor in self.valoresPosibles["G3"]:
 			p[valor] = self.calcularProbabilidad(ejemplo, probabilidades, probabilidadesCondicionadas, valor)
 
 		return max(p.iteritems(), key=lambda (k, v): v)[0]
@@ -91,10 +109,10 @@ class Bayes:
 		Calcula la probabilidad de que un ejemplo tenga determinado 'valor' en el atributo objetivo
 		basandose en las probabilidades previamente calculadas
 		'''
-
 		probabilidad = probabilidades[self.atributoObjetivo][valor]
 
 		for (k, v) in ejemplo.iteritems():
-			probabilidad = float(probabilidad*probabilidades[k][v]*probabilidadesCondicionadas[k][v][valor])
+			if k in self.atributos:
+				probabilidad = probabilidad*probabilidades[k][v]*probabilidadesCondicionadas[k][v][valor]
 
 		return probabilidad
--- Practico3/main.py
+++ Practico3/main.py
@@ -1,6 +1,7 @@
 from estudiante import Atributo,EstudianteBuilder
-from arbol import *
-from id3 import Id3
+# from arbol import *
+# from id3 import Id3
+from bayes import Bayes
 from random import shuffle
 # from dibujante import Dibujante # Utilizado para dibujar el arbol. Requiere bibliotecas extra
 
@@ -39,32 +40,25 @@ print(str(len(estudiantes)) + " estudiantes cargados")
 # Se genera la lista de atributos a considerar en el entrenamiento, partiendo de todos
 # los atributos definidos en el builder, y eliminando los que no queremos usar
 atributos = builder.atributos.keys()
-atributos.remove("G3")
+#atributos.remove("G3")
 atributos.remove("G2")
 atributos.remove("G1")
 atributos.remove("age")
 
 # Se genera una instancia del algoritmo pasandole el atributo objetivo
-id3 = Id3("G3",valoresPosibles)
+# id3 = Id3("G3",valoresPosibles)
+bayes = Bayes("G3", atributos, valoresPosibles)
 
-# Ejecutamos una vez el algoritmo entrenando con todos los estudiantes para obtener
-# unas estadisticas iniciales del arbol resultante
-arbol = id3.ejecutar(estudiantes, atributos)
-arbol.imprimirEstadisticas()
-print("Relacion hojas/ejemplos: " + str(float(arbol.cantidadHojas())/len(estudiantes)))
-print("Relacion hojas aprendidas/ejemplos: " + str(float(arbol.cantidadHojasAprendidas())/len(estudiantes)))
-arbol.podar()
-print("Podado del arbol")
-arbol.imprimirEstadisticas()
-print("Relacion hojas/ejemplos: " + str(float(arbol.cantidadHojas())/len(estudiantes)))
-print("Relacion hojas aprendidas/ejemplos: " + str(float(arbol.cantidadHojasAprendidas())/len(estudiantes)))
-# Dibujante.dibujar(arbol)
+
 
 # ------------------
-# VALIDACION CRUZADA
+# VALIDACION DE BAYES
 # ------------------
 
-print "Validacion cruzada..."
+
+# Se realizaran multiples pruebas para obtener una estimacion de la efectividad del algoritmo
+# para cualquier eleccion de casos de entrenamiento / prueba
+CANTIDAD_PRUEBAS = 100
 
 # Separo 1/5 de los casos de prueba
 cantEstTest = len(estudiantes)/5
@@ -74,51 +68,17 @@ shuffle(estudiantes)
 estudiantesTest = estudiantes[:cantEstTest]
 estudiantesEntrenamiento = estudiantes[cantEstTest + 1:]
 
-# Realizo validacion cruzada sobre estudiantesEntrenamiento
-CANT_BLOQUES = 10
-
-largoBloque = len(estudiantesEntrenamiento)/10
-
-resultados = []
-
-for i in range(CANT_BLOQUES):
-    pos = i*largoBloque
-    estTest = estudiantesEntrenamiento[pos:pos+largoBloque]
-    estEntr = estudiantesEntrenamiento[:pos] + estudiantesEntrenamiento[pos+largoBloque:]
-    arbol = id3.ejecutar(estEntr,atributos)
-    resultados.append(arbol.validarLista(estTest, "G3"))
-
-aciertos = sum([r/CANT_BLOQUES for r in resultados])
-print ("Error medio: " + str(1 - aciertos))
-
-
-# ------------------
-# EVALUACION FINAL
-# ------------------
-
-print "Evaluacion final..."
-
-# Entreno el arbol con estudiantesEntrenamiento y lo valido con estudiantesTest
-arbol = id3.ejecutar(estudiantesEntrenamiento, atributos)
-aciertos = arbol.validarLista(estudiantesTest, "G3")
-print "Error: " + str(1 - aciertos)
-
-
-
-# Se realizaran multiples pruebas para obtener una estimacion de la efectividad del algoritmo
-# para cualquier eleccion de casos de entrenamiento / prueba
-CANTIDAD_PRUEBAS = 25
-
 cantEstTest = len(estudiantes)/5
 CANT_BLOQUES = 10
 largoBloque = len(estudiantesEntrenamiento)/10
 
+validacionMasProbable = 0
 validacionCruzada = 0
 validacionFinal = 0
-validacionPodas = {}
-MAX_RAMAS = 10
-for i in range(1, MAX_RAMAS):
-    validacionPodas[i] = 0
+validacionAjuste = {}
+MAX_AJUSTE = 5
+for i in range(1, MAX_AJUSTE):
+    validacionAjuste[i] = 0
 
 for prueba in range(CANTIDAD_PRUEBAS):
 
@@ -126,29 +86,47 @@ for prueba in range(CANTIDAD_PRUEBAS):
     estudiantesTest = estudiantes[:cantEstTest]
     estudiantesEntrenamiento = estudiantes[cantEstTest + 1:]
 
+    # Obtengo porcentaje de aciertos sugiriendo opcion mas probable
+    cant = {}
+    for v in valoresPosibles["G3"]:
+        cant[v] = len([e for e in estudiantesEntrenamiento if e["G3"] == v])
+    masProbable = max(cant.iteritems(), key=lambda (k, v): v)[0]
+    correctos = 0
+    for e in estudiantesTest:
+        if e["G3"] == masProbable:
+            correctos += 1
+    validacionMasProbable += float(correctos)/len(estudiantesTest)
+
     resultados = []
 
     for i in range(CANT_BLOQUES):
         pos = i*largoBloque
         estTest = estudiantesEntrenamiento[pos:pos+largoBloque]
         estEntr = estudiantesEntrenamiento[:pos] + estudiantesEntrenamiento[pos+largoBloque:]
-        arbol = id3.ejecutar(estEntr,atributos)
-        resultados.append(arbol.validarLista(estTest, "G3"))
+        # arbol = id3.ejecutar(estEntr,atributos)
+        # resultados.append(arbol.validarLista(estTest, "G3"))
+        (p, pc) = bayes.ejecutar(ejemplos= estEntr)
+        resultados.append(bayes.validarLista(estTest, p, pc))
 
     validacionCruzada += sum([r/CANT_BLOQUES for r in resultados])
 
 
     # Entreno el arbol con estudiantesEntrenamiento y lo valido con estudiantesTest
-    arbol = id3.ejecutar(estudiantesEntrenamiento, atributos)
-    validacionFinal += arbol.validarLista(estudiantesTest, "G3")
+    # arbol = id3.ejecutar(estudiantesEntrenamiento, atributos)
+    # validacionFinal += arbol.validarLista(estudiantesTest, "G3")
+    (p, pc) = bayes.ejecutar(ejemplos= estudiantesEntrenamiento)
+    validacionFinal += bayes.validarLista(estudiantesTest, p, pc)
 
-    for i in range(1, MAX_RAMAS):
-        arbol = id3.ejecutar(estudiantesEntrenamiento, atributos, maxDepth = i)
-        validacionPodas[i] += arbol.validarLista(estudiantesTest, "G3")
+    for i in range(1, MAX_AJUSTE):
+        # arbol = id3.ejecutar(estudiantesEntrenamiento, atributos, maxDepth = i)
+        # validacionAjuste[i] += arbol.validarLista(estudiantesTest, "G3")
+        (p, pc) = bayes.ejecutar(ejemplos= estudiantesEntrenamiento, ajuste= i)
+        validacionAjuste[i] += bayes.validarLista(estudiantesTest, p, pc)
 
 
-print 'Porcentaje de aciertos aciertos:'
+print 'Porcentaje de aciertos:'
+print 'Valor mas probable: ' + str(100*validacionMasProbable/CANTIDAD_PRUEBAS)
 print 'Validacion Cruzada: ' + str(100*validacionCruzada/CANTIDAD_PRUEBAS)
-print 'Evaluacion sin poda: ' + str(100*validacionFinal/CANTIDAD_PRUEBAS)
-for i in range(1,MAX_RAMAS):
-    print 'Evaluacion profundidad ' + str(i) + ': ' + str(100*validacionPodas[i]/CANTIDAD_PRUEBAS)
+print 'Evaluacion por defecto: ' + str(100*validacionFinal/CANTIDAD_PRUEBAS)
+for i in range(1,MAX_AJUSTE):
+    print 'Evaluacion profundidad ' + str(i) + ': ' + str(100*validacionAjuste[i]/CANTIDAD_PRUEBAS)
